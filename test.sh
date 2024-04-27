cfd57190c129c3ee8ea5f37b912b3b5c4965b665
Tom <73077675+tmzane@users.noreply.github.com>
2024-04-27 22:20:51 +0500

    test: improve functional tests (#26)

--- test.sh
+++ test.sh
@@ -8,13 +8,27 @@ if [ "$os" = "windows-latest" ]; then
     exe=".exe"
 fi
 
-version="1.18"
+dump_version() {
+    file="$1"
+    hash -r # refresh binary paths
+    go version | awk '{print $3}' > "$file"
+}
+
 go build -o goversion"$exe"
+
+version="1.18"
+echo "go$version" > want
+dump_version main
+
 echo "Switching to $version"
 ./goversion"$exe" use "$version"
+dump_version got
+diff got want
+
 echo "Installed versions"
 ./goversion"$exe" ls
-hash -r # refresh binary paths
-go version | awk '{print $3}' > got
-echo "go$version" > want
-diff got want
+
+echo "Removing $version"
+./goversion"$exe" rm "$version"
+dump_version got
+diff got main
