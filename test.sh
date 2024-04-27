3d1c06b4c2f0cc75f4b9af820ee533bbf411dcd3
Tom <73077675+tmzane@users.noreply.github.com>
2024-04-27 16:35:30 +0500

    refresh binary paths

--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ fi
 go build -o goversion"$exe"
 ./goversion"$exe" use 1.18
 ./goversion"$exe" ls
+hash -r # refresh binary paths
 go version | awk '{print $3}' > got
 echo "go1.18" > want
 diff got want
